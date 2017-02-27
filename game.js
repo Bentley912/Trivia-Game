a1663d99023458acd8f71f8e685b8031eaa3543f
Patrick <pbentley07@gmail.com>
2017-02-26 21:57:46 -0500

    Coded crude timer and question div

---
+++ game.js
@@ -0,0 +1,52 @@
+// create html page that has all the questions in seperate divs
+
+// starting page with "Start" Button
+
+$('#timeUpGif').hide();
+
+
+
+$('#start').on("click", function(){
+    $('.question-section').empty();
+});
+
+var stopwatch = {
+  time:30,
+  count: function(){
+    stopwatch.time--;
+    $('.clock').html(stopwatch.time);
+    },
+    start: function() {
+      
+        intervalId = setInterval(stopwatch.count, 1000);
+    }
+
+
+}
+console.log(stopwatch.time);
+
+$('#timer').click(stopwatch.start);
+
+
+
+
+
+
+
+
+// When start button is clicked first question appears.
+
+// Timer also appears in the main section and begins counting down
+
+// if timer counts down to 0, the question is marked incorrect (incorrect ++) and a gif is shown and user notified that the time ran out
+
+// if the wrong answer is clicked , incorrect ++, and gif shows
+
+//if answer is correct, correct ++, and gif shows
+
+// when gif shows, it shoudl run for 5 seconds and then push next section into main division
+
+
+// when last question is answered, final page should show correct and incorrect answers
+
+// final page should have button that restarts trivia game at first question.
